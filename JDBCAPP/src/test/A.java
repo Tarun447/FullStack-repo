0d26e8cf34360326e3b2321c75506e15adb2893d
Tarun <tarunkumarnanda5@gmail.com>
2021-06-27 20:46:31 +0530

    Jdbc App Created

--- JDBCAPP/src/test/A.java
+++ JDBCAPP/src/test/A.java
@@ -1,10 +1,30 @@
 package test;
 
+import java.sql.*;
 public class A {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try
+		{
+			Connection con = Dao.getConnection();
+			// Prepare Statement 
+			PreparedStatement ps = con.prepareStatement("select * from car");
+			ResultSet rs = ps.executeQuery();
+			System.out.println("============Show Content========");
+			while(rs.next())
+			{
+				System.out.println(rs.getString(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3));
+				
+			}
+			System.out.println("===============================");
+			con.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			
+		}
+		
 	}
 
 }
